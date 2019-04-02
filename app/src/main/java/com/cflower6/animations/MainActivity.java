556ee42aa97520529249acccd46e71d8c233fd7b
Chris Flowers <christopher.j.flowers6@gmail.com>
2019-04-02 12:49:01 -0400

    more simple animation fun

--- app/src/main/java/com/cflower6/animations/MainActivity.java
+++ app/src/main/java/com/cflower6/animations/MainActivity.java
@@ -8,16 +8,26 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
     public void fade(View view){
         ImageView goku = (ImageView) findViewById(R.id.goku);
         ImageView gokuSSJ = (ImageView) findViewById(R.id.ssj);
-        goku.animate().alpha(0f).setDuration(2000);
-        gokuSSJ.animate().alpha(1f).setDuration(2000);
+
+        goku.animate().translationXBy(3000f).setDuration(2000);
+//        goku.animate().alpha(0f).setDuration(2000);
+        gokuSSJ.animate().alpha(0f).setDuration(2000);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        ImageView goku = (ImageView)findViewById(R.id.goku);
+
+        goku.setTranslationX(-3000f);
+        */
     }
 }
